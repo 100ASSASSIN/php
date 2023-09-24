5e6ea3da056fa729ae406d5078b0f728f627b70d
ASSASSIN <127267168+100ASSASSIN@users.noreply.github.com>
2023-09-25 01:02:09 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <header>
+    <?php
+echo $name = "akash <br>";
+echo $a = 100;
+echo $b =100;
+echo"assassin";
+?>
+    <body bgcolor="red";>
+        <h1>ASSASSIN</h1>
+</body>
+
+</html>
